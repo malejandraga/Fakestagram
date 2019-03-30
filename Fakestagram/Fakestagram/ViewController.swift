57979af970f46a0f18ffb96dba71b4243137de79
Maria Alejandra Gutierrez Ahumada <magutierreza_diplomado@MacBook-19.local>
2019-03-30 10:43:51 -0600

    First commit

---
+++ Fakestagram/Fakestagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fakestagram
+//
+//  Created by Maria Alejandra Gutierrez Ahumada D3 on 3/30/19.
+//  Copyright © 2019 Maria Alejandra Gutierrez Ahumada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
